a24c2ad52e6a720aa223cda09c06764434cfaa0e
gautam <gautamsharma.best@gmail.com>
2023-01-31 22:19:49 +0530

    Day 9

---
+++ 1207. Unique Number of Occurrences.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+       vector<int> freq;
+        
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<arr.size();i++){
+            int count =0;
+            for(int j=i+1;j<arr.size();j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                }
+                    else{
+                        break;
+                    }
+                }
+                freq.push_back(count);
+                i=i+count;
+            }
+            for(int v=0;v<freq.size();v++){
+                cout<<freq[v];
+            }
+            sort(freq.begin(),freq.end());
+            for(int i=0;i<freq.size()-1;i++){
+                if(freq[i]==freq[i+1])
+                    return false;
+            }
+         return true;
+        }
+};
---
+++ 24. Swap Nodes in Pairs.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* &head) {
+        if(head==NULL){
+            return NULL;
+        }
+        int pair=2;
+        int count=0;
+        ListNode* prev=NULL;
+        ListNode* current=head;
+        ListNode* temp;
+        while(current!=NULL && count<pair){
+            temp=current->next;
+            current->next=prev;
+            prev=current;
+            current=temp;
+            count++;
+        }
+        cout<<"hi";
+        if(temp!=NULL){
+            head->next=swapPairs(temp);
+        }
+        return prev;
+    }
+};
