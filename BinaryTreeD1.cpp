8e18b75c5933aaffd5d0515940cb470d2daaa213
gautam <gautamsharma.best@gmail.com>
2023-02-11 00:45:13 +0530

    Day 17

--- BinaryTreeD1.cpp
+++ BinaryTreeD1.cpp
@@ -15,23 +15,44 @@ public:
 };
 
 node* createTree(node* root){
-    cout << "Enter Data : "<<endl;
     int data;
-
-    int rightdata;
+    cout << "Enter value ";
     cin >> data;
     root = new node(data);
     if(data==-1){
         return NULL; 
     }
-    cout << "Enter Values for left :";
+    cout << "for left :"<<data<<endl;
     root->left = createTree(root->left);
-    cout << "Enter Values for right :";
+    cout << "for right :"<<data<<endl;
     root->right = createTree(root->right);
     return root;
+}
+//1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+void levelOrder(node* root){
+    queue<node*> q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        node* temp=q.front();
+        if(temp==NULL){
+            cout<<endl;
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }else{
+            if(temp->left){
+                q.push(temp->left);
+            }
+            if(temp->right){
+                q.push(temp->right);
+            }
+        }
+    }
 }
 int main(){
     node* root=NULL;
     root=createTree(root);
+    levelOrder(root);
     return 0;
 }
