9afa7cae28c45d1c8db207e2ae9a8aa5a9b235e0
gautam <gautamsharma.best@gmail.com>
2023-02-12 23:13:11 +0530

    Compiler Changes

--- 24. Swap Nodes in Pairs.cpp
+++ 24. Swap Nodes in Pairs.cpp
@@ -1,15 +1,16 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+#include<iostream>
+using namespace std;
+
 class Solution {
 public:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
     ListNode* swapPairs(ListNode* &head) {
         if(head==NULL){
             return NULL;
