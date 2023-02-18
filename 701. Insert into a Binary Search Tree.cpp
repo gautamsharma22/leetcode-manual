9afe922a80ef68d58f6d3d497086b60a2ec71894
gautam <gautamsharma.best@gmail.com>
2023-02-19 01:06:58 +0530

    Day 24

---
+++ 701. Insert into a Binary Search Tree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==NULL)return new TreeNode(val);
+        TreeNode* temp=root;
+        while(true){
+            if(temp->val > val){
+                if(temp->left)temp=temp->left;
+                else {
+                    TreeNode* insert=new TreeNode(val);
+                    temp->left=insert;
+                    break;
+                }
+            }else{
+                if(temp->right)temp=temp->right;
+                else {
+                    TreeNode* insert=new TreeNode(val);
+                    temp->right=insert;
+                    break;
+                }
+            }
+        }
+        return root;
+    }
+};
