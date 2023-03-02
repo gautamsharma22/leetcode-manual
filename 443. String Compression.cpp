bdf2085b02d21d2d7c08cec42806be68c73a6b04
gautam <gautamsharma.best@gmail.com>
2023-03-02 11:29:37 +0530

    Day 35

---
+++ 443. String Compression.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int n=chars.size();
+        if(n==0 || n==1)return n;
+        int i=0;
+        int j=1;
+        int last=1;
+        while(i<n){
+            while( j < n && chars[i]==chars[j]){
+                j++;
+            }
+            int count=j-i;
+            if(i>0)chars[last++]=chars[i];
+            if (count > 1) {
+                string str = to_string(count);
+                for (char c : str) {
+                    chars[last++]=c;
+                }
+            }
+            i=j;
+        }
+        return last;
+    }
+};
