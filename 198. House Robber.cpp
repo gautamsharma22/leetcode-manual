ca6b55d145357b7b8168c522949d974ddf22b17b
gautam <gautamsharma.best@gmail.com>
2023-03-14 00:49:59 +0530

    Day 47

--- 198. House Robber.cpp
+++ 198. House Robber.cpp
@@ -51,8 +51,8 @@ public:
         if(n==1)return nums[0];
         if(n==2)return max(nums[0],nums[1]);
         vector<int> dp(n,0);
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
+        dp[0] = 0;
+        dp[1] = max(nums[0], nums[1]);
         for(int i=2;i<n;i++){
             int include=nums[i]+dp[i-2];
             int exclude=0+dp[i-1];
@@ -69,9 +69,10 @@ public:
         int n=nums.size();
         if(n==1)return nums[0];
         if(n==2)return max(nums[0],nums[1]);
-        int prev1=nums[0];
-        int prev2=max(nums[0],nums[1]);
-        for(int i=2;i<n;i++){
+        int prev1 = 0;
+        int prev2 = 0;
+        for (int i = 0; i < n; i++)
+        {
             int include=nums[i]+prev1;
             int exclude=0+prev2;
             prev1=prev2;
