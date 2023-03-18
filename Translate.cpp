e501282541890027d0c4627f13b18f147c270b00
gautam <gautamsharma.best@gmail.com>
2023-03-18 22:15:40 +0530

    Day 50

--- Translate.cpp
+++ Translate.cpp
@@ -8,7 +8,7 @@ void main()
     int x1, y1, x2, y2, x3, y3, i, j, k, tx, ty;
     initgraph(&gd, &gm, "C:\\TURBOC3\\BGI");
     scanf("%d%d%d%d%d%d", &x1, &y1, &x2, &y2, &x3, &y3);
-    scanf("%d%d", &tx , &ty);
+    scanf("%d%d", &tx, &ty);
     op[0][0] = op[1][1] = op[2][2] = 1;
     op[0][1] = op[1][0] = op[2][0] = op[2][1] = 0;
     op[0][2] = tx;
@@ -17,7 +17,7 @@ void main()
     ob[1][0] = y1;
     ob[0][1] = x2;
     ob[1][1] = y2;
-    ob[0][2] = x3;cvbn
+    ob[0][2] = x3;
     ob[1][2] = y3;
     ob[2][0] = ob[2][1] = ob[2][2] = 1;
     line(x1, y1, x2, y2);
