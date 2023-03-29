871fa67ec8703eb35b22c97475cef18153c7dbda
gautam <gautamsharma.best@gmail.com>
2023-03-30 00:20:08 +0530

    Day 61

--- 96. Unique Binary Search Trees.cpp
+++ 96. Unique Binary Search Trees.cpp
@@ -1,20 +1,20 @@
 class Solution
 {
 public:
-    // int helper(int n){
-    //     if(n<=1)return 1;
-    //     int ans=0;
-    //     for(int i=1;i<=n;i++){
-    //         int left=helper(i-1);
-    //         int right=helper(n-i);
-    //         ans+=left*right;
-    //     }
-    //     return ans;
-    // }
-    // int numTrees(int n) {
-    //     return helper(n);
-    // }
-    int helper(int n, vector<int> &dp)
+    int numTrees_Recursion(int n)
+    {
+        if (n <= 1)
+            return 1;
+        int ans = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            int left = numTrees_Recursion(i - 1);
+            int right = numTrees_Recursion(n - i);
+            ans += left * right;
+        }
+        return ans;
+    }
+    int numTrees_Memoization(int n, vector<int> &dp)
     {
         if (n <= 1)
             return 1;
@@ -23,15 +23,35 @@ public:
         int ans = 0;
         for (int i = 1; i <= n; i++)
         {
-            int left = helper(i - 1, dp);
-            int right = helper(n - i, dp);
+            int left = numTrees_Memoization(i - 1, dp);
+            int right = numTrees_Memoization(n - i, dp);
             ans += left * right;
         }
         return dp[n] = ans;
     }
+    int numTrees_Tabulation(int n)
+    {
+        vector<int> dp(n + 1, 0);
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                int left = dp[j - 1];
+                int right = dp[i - j];
+                dp[i] += left * right;
+            }
+        }
+        return dp[n];
+    }
+    // Simply calculate Catlan number...
     int numTrees(int n)
     {
-        vector<int> dp(n + 1, -1);
-        return helper(n, dp);
+        long Catalan = 1;
+        for (int i = 0; i < n; ++i)
+        {
+            Catalan = Catalan * 2 * (2 * i + 1) / (i + 2);
+        }
+        return Catalan;
     }
 };
