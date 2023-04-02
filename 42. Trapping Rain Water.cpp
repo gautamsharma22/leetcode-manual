29739d06245cff4fc7f9c82316d55fab2252cf91
gautam <gautamsharma.best@gmail.com>
2023-04-03 00:00:14 +0530

    Day 63

--- 42. Trapping Rain Water.cpp
+++ 42. Trapping Rain Water.cpp
@@ -32,4 +32,52 @@ public:
         }
         return sum;
     }
+
+    /*
+        Approach :
+        This approach is based on two pointers and instead of calculating prefix sum array array and postfix sum we reduce it to two variables say leftMax and rightMax.
+
+        1) Initialize maxLeft,maxRight,left,right.(We will start from starting index+1 and go to ending index-1 because boundary values cant store any water.)
+        2) Check minimum of leftMax and rightMax, if left is minimum ->
+        2) Calculate the maxLeft-heightLeft (Empty Space).
+        3) Store value in total (trappedWater).
+        4) Update max left if greater value found.
+        5) Update Left+1.
+        6) Do same thing for right also.
+
+        Reason this approach works because at starting we are checking min of maxLeft and maxRight and taking whichever is minimum by this we ensure that the at any position the maximum value any index can store should be min of (maxRight,maxLeft). the same thing used here.
+
+        Suppose in maxLeft=2 maxRight=3; In any case boundary between any two point comes out 10 so if we are moving from left we would only store 2(Greater than 2 will cause spilling of water).
+    */
+    int trapSpaceOptimised(vector<int> &height)
+    {
+        int n = height.size();
+        int maxLeft = height[0];
+        int maxRight = height[n - 1];
+        int left = 1;
+        int right = n - 2;
+        int trappedWater = 0;
+        while (left <= right)
+        {
+            if (maxLeft <= maxRight)
+            {
+                int x = maxLeft - height[left];
+                if (x > 0)
+                    trappedWater += x;
+                if (height[left] > maxLeft)
+                    maxLeft = height[left];
+                left++;
+            }
+            else
+            {
+                int x = maxRight - height[right];
+                if (x > 0)
+                    trappedWater += x;
+                if (height[right] > maxRight)
+                    maxRight = height[right];
+                right--;
+            }
+        }
+        return trappedWater;
+    }
 };
