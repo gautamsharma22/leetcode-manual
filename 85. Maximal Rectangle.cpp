0664112dcdac458cb65a5ae46d5c14d82c68ee66
gautam <gautamsharma.best@gmail.com>
2023-04-06 02:08:12 +0530

    Day 66

--- 85. Maximal Rectangle.cpp
+++ 85. Maximal Rectangle.cpp
@@ -1,77 +1,85 @@
-class Solution {
-    private:
-        vector<int> nextsmaller(vector<int>& heights,int n){
+class Solution
+{
+public:
+    // Below is the function from Largest area in histogram which provides maximum area by taking heights array.
+    vector<int> nextSmallerElement(vector<int> arr)
+    {
+        int n = arr.size();
+        vector<int> ans(n);
         stack<int> s;
         s.push(-1);
-        vector<int> ans(n);
-        for(int i=n-1;i>=0;i--){
-            while( s.top()!=-1 && heights[s.top()]>=heights[i]){
+        for (int i = n - 1; i >= 0; i--)
+        {
+            while (s.top() != -1 && arr[s.top()] >= arr[i])
+            {
                 s.pop();
             }
-            if(!s.empty()){
-                ans[i]=s.top();
-                s.push(i);
-            }
-            else{
-                ans[i]=heights[i];
-                s.push(heights[i]);
-            } 
-            
+            ans[i] = s.top();
+            s.push(i);
         }
         return ans;
     }
-    vector<int> prevsmaller(vector<int>& heights,int n){
+    vector<int> prevSmallerElement(vector<int> arr)
+    {
+        int n = arr.size();
+        vector<int> ans(n);
         stack<int> s;
         s.push(-1);
-        vector<int> ans(n);
-            for(int i=0;i<n;i++){
-                while(s.top()!=-1 && heights[s.top()]>=heights[i]){
-                    s.pop();
-                }
-                if(!s.empty()){
-                    ans[i]=s.top();
-                    s.push(i);
-                }else{
-                    ans[i]=heights[i];
-                    s.push(heights[i]);
-                }
-                
+        for (int i = 0; i < n; i++)
+        {
+            while (s.top() != -1 && arr[s.top()] >= arr[i])
+            {
+                s.pop();
             }
+            ans[i] = s.top();
+            s.push(i);
+        }
         return ans;
     }
-    int largestRectangleArea(vector<int>& heights) {
-        int n=heights.size();
-        vector<int> next=nextsmaller(heights,n);
-        vector<int> prev=prevsmaller(heights,n);
-        int newarea=0;
-        for(int i=0;i<n;i++){
-            int length=heights[i];
-            if(next[i]==-1){
-                next[i]=n;
-            }
-            int breadth=next[i]-prev[i]-1;
-            int area=length*breadth;
-            newarea=max(area,newarea);
+    int largestRectangleArea(vector<int> &heights)
+    {
+        vector<int> next;
+        vector<int> prev;
+        next = nextSmallerElement(heights);
+        prev = prevSmallerElement(heights);
+        int maxarea = 0;
+        for (int i = 0; i < heights.size(); i++)
+        {
+            int length = heights[i];
+            if (next[i] == -1)
+                next[i] = heights.size();
+            int width = next[i] - prev[i] - 1;
+            int area = length * width;
+            maxarea = max(maxarea, area);
         }
-        return newarea;
+        return maxarea;
     }
-public:
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        int maxarea=0;
-        vector<int> matrix1(matrix[0].size(), 0);
-
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[i].size();j++){
-                if(matrix[i][j]=='0'){
-                    matrix1[j]=0;
-                }else{
-                    matrix1[j]++;
+    /*
+    Approach :
+    To solve this we have to just convert this problem to largest Area in histogram Problem.
+    Everything is same (bars logic added).
+    */
+    int maximalRectangle(vector<vector<char>> &matrix)
+    {
+        int m = matrix[0].size();
+        vector<int> bars(m, 0);
+        int maxarea = 0;
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (matrix[i][j] == '1')
+                {
+                    bars[j] += 1; // Instaed of creating bars every time we just update the value;
+                }
+                else
+                {
+                    bars[j] = 0; // Means we have to reset the bar.
                 }
             }
-            int area=largestRectangleArea(matrix1);
-            maxarea=max(area,maxarea);
+            int area = largestRectangleArea(bars);
+            maxarea = max(maxarea, area);
         }
-
         return maxarea;
     }
 };
