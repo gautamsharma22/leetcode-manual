41820e6f74ba27e93dd6d3130ae559eecf356a32
gautam <gautamsharma.best@gmail.com>
2023-04-07 00:28:21 +0530

    Day 66

--- 44. Wildcard Matching.cpp
+++ 44. Wildcard Matching.cpp
@@ -48,4 +48,125 @@ public:
     {
         return helper(s, p, 0, 0);
     }
+    int helper_Memoization(string &s, string &p, int i, int j, vector<vector<int>> &dp)
+    {
+        if (i == s.size() && j == p.size())
+            return true;
+        if (i <= s.size() && j >= p.size())
+            return false;
+        if (i >= s.size() && j < p.size())
+        {
+            for (int i = j; i < p.size(); i++)
+            {
+                if (p[i] != '*')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        if (s[i] == p[j] || p[j] == '?')
+        {
+            return dp[i][j] = helper_Memoization(s, p, i + 1, j + 1, dp);
+        }
+        else if (p[j] == '*')
+        {
+            return dp[i][j] = (helper_Memoization(s, p, i + 1, j, dp) || helper_Memoization(s, p, i, j + 1, dp));
+        }
+        else
+        {
+            return dp[i][j] = false;
+        }
+    }
+    bool isMatch(string s, string p)
+    {
+        int n = s.size();
+        int m = p.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        return helper_Memoization(s, p, 0, 0, dp);
+    }
+    bool isMatch_Tabulation(string s, string p)
+    {
+        int n = s.size();
+        int m = p.size();
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+        dp[n][m] = true;
+        for (int i = 0; i <= p.size(); i++)
+        {
+            bool notfound = true;
+            for (int k = i; k < p.size(); k++)
+            {
+                if (p[k] != '*')
+                {
+                    notfound = false;
+                    break;
+                }
+            }
+            dp[n][i] = notfound;
+        }
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = m - 1; j >= 0; j--)
+            {
+                if (s[i] == p[j] || p[j] == '?')
+                {
+                    dp[i][j] = dp[i + 1][j + 1];
+                }
+                else if (p[j] == '*')
+                {
+                    dp[i][j] = (dp[i + 1][j] || dp[i][j + 1]);
+                }
+                else
+                {
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[0][0];
+    }
+    bool isMatch_SpaceOptimised(string s, string p)
+    {
+        int n = s.size();
+        int m = p.size();
+        vector<bool> next(m + 1, false);
+        vector<bool> curr(m + 1, false);
+        next[m] = true;
+        for (int i = 0; i <= p.size(); i++)
+        {
+            bool notfound = true;
+            for (int k = i; k < p.size(); k++)
+            {
+                if (p[k] != '*')
+                {
+                    notfound = false;
+                    break;
+                }
+            }
+            next[i] = notfound;
+        }
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = m - 1; j >= 0; j--)
+            {
+                if (s[i] == p[j] || p[j] == '?')
+                {
+                    curr[j] = next[j + 1];
+                }
+                else if (p[j] == '*')
+                {
+                    curr[j] = (next[j] || curr[j + 1]);
+                }
+                else
+                {
+                    curr[j] = false;
+                }
+            }
+            next = curr;
+        }
+        return next[0];
+    }
 };
+}
+;
