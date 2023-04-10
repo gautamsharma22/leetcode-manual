5eccabca92e04caec08b6d66f66b36f64a7f8e1e
gautam <gautamsharma.best@gmail.com>
2023-04-11 00:21:57 +0530

    Day 70

--- 403. Frog Jump.cpp
+++ 403. Frog Jump.cpp
@@ -16,6 +16,8 @@ public:
             * Create a hashmap of [ stones[i] -> { From how many jumps we can reach here } ]
             * If last stone set is non empty means from any jump we have reached there.
             * Return if(lastStoneSet is empty or not).
+            * In this approach it may seem that why we are adding jumps for stones(which may have not been present in the our source array -> for that if we we'll check if stones[i]+jump is valid or not our time complexity may increase (search in set is 0(1) but in worst case it may be 0(n)) ).
+            * We can do searching approach also.
 
     */
     bool Recursive_helper(vector<int> &stones, int index, int jump)
@@ -48,18 +50,20 @@ public:
     }
     bool canCross_HashMap(vector<int> &stones)
     {
+        // Use Set to ignore duplicate values.
         unordered_map<int, unordered_set<int>> mapp;
 
         int n = stones.size();
+        // From stone[0]->stone[0]+1 we have taken 1 Jumps so Add it.
         mapp[stones[0] + 1] = {1};
         for (int i = 1; i < n; i++)
         {
             int stone = stones[i];
-            for (auto a : mapp[stone])
+            for (auto jumps : mapp[stone]) // From how many jumps we can reach here
             {
-                mapp[stone + a].insert(a);
-                mapp[stone + (a + 1)].insert(a + 1);
-                mapp[stone + (a - 1)].insert(a - 1);
+                mapp[stone + jumps].insert(jumps);
+                mapp[stone + (jumps + 1)].insert(jumps + 1);
+                mapp[stone + (jumps - 1)].insert(jumps - 1);
             }
         }
         return mapp[stones[n - 1]].size() != 0;
