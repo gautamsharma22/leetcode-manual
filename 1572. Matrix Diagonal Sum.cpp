bd7b8ff0829a73bde88d8d7bcb2a44c6062bf5ac
gautam <gautamsharma.best@gmail.com>
2023-05-08 23:08:32 +0530

    Day 84

---
+++ 1572. Matrix Diagonal Sum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int sum=0;
+        for(int i=0,j=n-1;i<n && j>=0;i++,j--){
+            sum+=mat[i][i];
+            if(!(i==j))sum+=mat[i][j];
+        }
+        return sum;
+    }
+};
